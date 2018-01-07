6a6a1daafe4c36e594d1a03e482d39e03daa2c98
wlanboy <wlanboy@users.noreply.github.com>
2018-01-07 14:06:31 +0100

    Delete build-and-start.sh
    Removed bash script. Moved this to readme.md.

--- build-and-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-mvn package -DskipTests=true
-docker build -t servicelogs:latest . --build-arg JAR_FILE=./target/ServiceLogs-0.0.1-SNAPSHOT.jar
-docker run --name servicelogs -d -p 9411:9411 --link serviceregistry:serviceregistry -v /tmp:/tmp servicelogs:latest
