90b01f7e3189b5357e86c4e1dad144c869425fc0
wlanboy <wlanboy@users.noreply.github.com>
2018-08-31 19:30:08 +0200

    Springboot20 (#1)
    * Update pom for Spring Boot Version 2.0.4
    
    * Fixing name change of Zipkin package names.
    
    * Fixing Zipkin depencencies

--- src/main/java/com/wlanboy/zipkin/server/ZipkinServiceApplication.java
+++ src/main/java/com/wlanboy/zipkin/server/ZipkinServiceApplication.java
@@ -14,44 +14,45 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 //import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
-import zipkin.server.EnableZipkinServer;
-import zipkin.server.RegisterZipkinHealthIndicators;
-import zipkin.server.ZipkinServer;
+import zipkin.server.internal.EnableZipkinServer;
+import zipkin.server.internal.RegisterZipkinHealthIndicators;
+//import zipkin.server.ZipkinServer;
 
 @EnableZipkinServer
 @EnableDiscoveryClient
 @EnableConfigurationProperties(MiwZipkinUiProperties.class)
 @SpringBootApplication
-public class ZipkinServiceApplication extends ZipkinServer {
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(ZipkinServiceApplication.class)
-            .listeners(new RegisterZipkinHealthIndicators())
-            .properties("spring.config.name=zipkin-server")
-            .run(args);
-    }
-    
-//    @Configuration
-//    public static class ZipkinServiceSecurityConfigurer extends ResourceServerConfigurerAdapter {
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            http.requestMatcher(new NegatedRequestMatcher(AnyRequestMatcher.INSTANCE));
-//        }
-//    }
-    
-//    @Configuration
-//    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 2)
-//    @EnableOAuth2Sso
-//    public static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//        @Autowired
-//        private MiwZipkinUiProperties properties;
-//        
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http
-//            .requestMatchers().antMatchers("/", "/login", "/zipkin/**").and()
-//            .headers().frameOptions().sameOrigin().and()
-//            .authorizeRequests()
-//                .anyRequest().hasAnyAuthority(properties.getAuthorizedRoles());
-//        }
-//    }
+public class ZipkinServiceApplication { // extends ZipkinServer
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZipkinServiceApplication.class).listeners(new RegisterZipkinHealthIndicators())
+				.properties("spring.config.name=zipkin-server").run(args);
+	}
+
+	// @Configuration
+	// public static class ZipkinServiceSecurityConfigurer extends
+	// ResourceServerConfigurerAdapter {
+	// @Override
+	// public void configure(HttpSecurity http) throws Exception {
+	// http.requestMatcher(new NegatedRequestMatcher(AnyRequestMatcher.INSTANCE));
+	// }
+	// }
+
+	// @Configuration
+	// @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 2)
+	// @EnableOAuth2Sso
+	// public static class WebSecurityConfiguration extends
+	// WebSecurityConfigurerAdapter {
+	// @Autowired
+	// private MiwZipkinUiProperties properties;
+	//
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// http
+	// .requestMatchers().antMatchers("/", "/login", "/zipkin/**").and()
+	// .headers().frameOptions().sameOrigin().and()
+	// .authorizeRequests()
+	// .anyRequest().hasAnyAuthority(properties.getAuthorizedRoles());
+	// }
+	// }
 }
